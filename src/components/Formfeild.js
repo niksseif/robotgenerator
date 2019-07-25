37585cb2b393702fada313230c7e4e9e624638b2
NikSS <nik.safe@gmail.com>
2019-07-25 11:04:06 -0700

    add remove card, auth , like , dislike and like counter

--- src/components/Formfeild.js
+++ src/components/Formfeild.js
@@ -5,103 +5,136 @@ import RoboCard from "./RoboCard";
 import "../App.scss";
 
 class FormFeild extends Component {
-  state = {
-    newRobot: {
-      url: [],
-      name: "",
-      job: ""
-    },
-    loading: true,
-    robo: false,
-    show: false
-  };
+    state = {
+        newRobot: {
+            url: [],
+            name: "",
+            job: ""
+        },
+        loading: true,
+        robo: false,
+        show: false,
+        auth: true
+    };
 
-  //handle submit
-  handleSubmit = async event => {
-    event.preventDefault();
-    const API_BASE_URL = await "https://robohash.org";
-    let newRobot = this.state.newRobot;
-    newRobot.url = [
-      ...newRobot.url,
-      `${API_BASE_URL}/${newRobot.name}/bgset=bg1 `
-    ];
-    this.setState({ ...newRobot, robo: true });
-    window.scrollTo(0, 480);
-    this.clear();
-  };
+    //handle submit
+    handleSubmit = async event => {
+        event.preventDefault();
+        const API_BASE_URL = await "https://robohash.org";
+        let newRobot = this.state.newRobot;
+        this.auth(newRobot.name, newRobot.url);
 
-  //clear input feild
-  clear = () => {
-    let newRobot = this.state.newRobot;
-    newRobot.name = "";
-    newRobot.job = "";
-    this.setState({ newRobot });
-  };
-  //hidden Alert
-  hiddenAlert = () => {
-    this.setState({ show: false });
-  };
+        if (this.state.auth) {
+            newRobot.url = [
+                ...newRobot.url,
+                `${API_BASE_URL}/${newRobot.name}/bgset=bg1 `
+            ];
+            this.setState({ ...newRobot, robo: true });
+            window.scrollTo(0, 480);
+            this.clear();
+        }
+    };
 
-  //render form
-  renderForm = () => {
-    return (
-      <div className="Form-container">
-        <Form className="form" onSubmit={this.handleSubmit}>
-          <Form.Field>
-            <label>What do you want to call your robot?</label>
-            <input
-              placeholder="Robot Name"
-              name="name"
-              value={this.state.newRobot.name}
-              onChange={e => {
-                let newRobot = { ...this.state.newRobot };
-                newRobot.name = e.target.value;
-                this.setState({ newRobot });
-              }}
-            />
-            <label>What your Robot do?</label>
-            <input
-              placeholder="What your robot do"
-              name="Robot job"
-              value={this.state.newRobot.job}
-              onChange={e => {
-                let newRobot = { ...this.state.newRobot };
-                newRobot.job = e.target.value;
-                this.setState({ newRobot });
-              }}
-            />
-          </Form.Field>
-          <Button type="submit" onClick={() => this.setState({ show: true })}>
-            Submit
-          </Button>
-        </Form>
-      </div>
-    );
-  };
-  render() {
-    const { newRobot, robo, show } = this.state;
-    return (
-      <div className="landing-container">
-        <span className="App-header"> ROBOT GENERATOR </span>
-        {this.renderForm()}
+    //clear input feild
+    clear = () => {
+        let newRobot = this.state.newRobot;
+        newRobot.name = "";
+        newRobot.job = "";
+        this.setState({ newRobot });
+    };
+    //hidden Alert
+    hiddenAlert = () => {
+        this.setState({ show: false });
+    };
 
-        <FlyingRobots />
-        {robo ? (
-          <div className="card-container">
-            {newRobot.url.map(item => {
-              return (
-                <RoboCard
-                  url={item}
-                  name={this.state.name}
-                  key={item}
-                  job={this.state.job}
-                />
-              );
-            })}
-          </div>
-        ) : null}
-      </div>
-    );
-  }
+    //remove card
+    removeCard = name => {
+        let res = this.state.newRobot.url.filter(item => {
+            if (!item.includes(name)) {
+                return item;
+            }
+        });
+        let newRobot = this.state.newRobot;
+        newRobot.url = res;
+        this.setState({ newRobot });
+    };
+    //handle  auth
+    auth = (name, url) => {
+        url.map(item => {
+            if (item.includes(name)) {
+                alert("this name exist, please enter another name");
+                this.setState({ auth: false });
+            } else {
+                alert("awesome you have not pick that name yet");
+                this.setState({ auth: true });
+            }
+        });
+    };
+
+    //render form
+    renderForm = () => {
+        return (
+            <div className="Form-container">
+                <Form className="form" onSubmit={this.handleSubmit}>
+                    <Form.Field>
+                        <label>What do you want to call your robot?</label>
+                        <input
+                            placeholder="Robot Name"
+                            name="name"
+                            value={this.state.newRobot.name}
+                            onChange={e => {
+                                let newRobot = { ...this.state.newRobot };
+                                newRobot.name = e.target.value;
+                                this.setState({ newRobot });
+                            }}
+                        />
+                        <label>What your Robot do?</label>
+                        <input
+                            placeholder="What your robot do"
+                            name="Robot job"
+                            value={this.state.newRobot.job}
+                            onChange={e => {
+                                let newRobot = { ...this.state.newRobot };
+                                newRobot.job = e.target.value;
+                                this.setState({ newRobot });
+                            }}
+                        />
+                    </Form.Field>
+                    <Button
+                        type="submit"
+                        onClick={() => this.setState({ show: true })}
+                    >
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        );
+    };
+    render() {
+        const { newRobot, robo } = this.state;
+        return (
+            <div className="landing-container">
+                <span className="App-header"> ROBOT GENERATOR </span>
+                {this.renderForm()}
+                <FlyingRobots />
+                {robo ? (
+                    <div className="card-container">
+                        {newRobot.url.map(item => {
+                            return (
+                                <RoboCard
+                                    url={item}
+                                    name={this.state.name}
+                                    key={item}
+                                    job={this.state.job}
+                                    remove={this.removeCard}
+                                    dislike={this.state.dislike}
+                                />
+                            );
+                        })}
+                    </div>
+                ) : null}
+            </div>
+        );
+    }
 }
 export default FormFeild;
--- src/components/RoboCard.js
+++ src/components/RoboCard.js
@@ -1,33 +1,81 @@
 import React, { Component } from "react";
-import { Card, Image } from "semantic-ui-react";
+import { Card, Image, Icon } from "semantic-ui-react";
 import "../App.scss";
 class RoboCard extends Component {
-  state = {
-    name: this.props.name,
-    job: this.props.job
-  };
- 
-  render() {
-    return (
-      <Card style={{ marginTop: "2%" }}>
-        {!this.props.url ? (
-          <div style={{ width: "20px", height: "20px" }}>Loading...</div>
-        ) : (
-          <Image
-            src={this.props.url}
-            wrapped
-            ui={false}
-            style={{ margin: "2%" }}
-          />
-        )}
-        <Card.Content>
-          <Card.Header>{this.state.name}</Card.Header>
-          <Card.Description>
-            {this.state.name} is a {this.state.job}.
-          </Card.Description>
-        </Card.Content>
-      </Card>
-    );
-  }
+    state = {
+        name: this.props.name,
+        job: this.props.job,
+        likeCount: 0,
+        heart: false,
+        comment: ""
+    };
+    addLike = event => {
+        let like = this.state.likeCount;
+        this.setState({ likeCount: (like += 1), heart: !this.state.heart });
+    };
+    removeLike = event => {
+        let like = this.state.likeCount;
+        this.setState({ likeCount: (like -= 1), heart: false });
+    };
+
+    render() {
+        return (
+            <Card style={{ marginTop: "2%" }}>
+                {!this.props.url ? (
+                    <div style={{ width: "20px", height: "20px" }}>
+                        Loading...
+                    </div>
+                ) : (
+                    <Image
+                        src={this.props.url}
+                        wrapped
+                        ui={false}
+                        style={{ margin: "2%" }}
+                    />
+                )}
+                <Card.Content>
+                    <Card.Header>{this.state.name}</Card.Header>
+                    <Card.Description>
+                        {this.state.name} is a {this.state.job}.
+                    </Card.Description>
+                    {/* <input
+              placeholder="What your robot do"
+            name="Robot job"
+            value={this.state.comment}
+            onChange={e => {
+              this.setState({ comment : e.target.value});
+            }}
+          >
+
+          </input> */}
+                    <Icon
+                        onClick={e => {
+                            e.preventDefault();
+                            this.props.remove(this.state.name);
+                        }}
+                        name="delete"
+                        style={{ float: "right" }}
+                    ></Icon>
+                    <Icon
+                        color={this.state.heart ? "red" : "grey"}
+                        name="bolt"
+                        style={{ float: "left" }}
+                        onClick={e => {
+                            this.addLike(e);
+                        }}
+                    ></Icon>
+                    <Icon
+                        color={!this.state.heart ? "blue" : "grey"}
+                        name="thumbs down outline"
+                        style={{ float: "left" }}
+                        onClick={e => {
+                            this.state.likeCount > 0 && this.removeLike(e);
+                        }}
+                    ></Icon>
+                    <span>{this.state.likeCount}</span>
+                </Card.Content>
+            </Card>
+        );
+    }
 }
 export default RoboCard;
